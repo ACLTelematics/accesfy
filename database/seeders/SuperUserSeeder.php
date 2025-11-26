09962738fa6fe8cd182782e0cc7dfeb9a73713a1
AdaKing <adaking@trxieniac.ipv6.infinitum.net.mx>
2025-11-25 21:05:25 -0500

    feat: add seeders for super_users, gym_owners, staff, and memberships

---
+++ database/seeders/SuperUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\SuperUser;
+
+class SuperUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        SuperUser::create([
+            'name' => 'acanche',
+            'email' => 'admin@accesfy.com',
+            'password' => Hash::make('@050554'),
+            'active' => true,
+        ]);
+
+        SuperUser::create([
+            'name' => 'admin',
+            'email' => 'support@accesfy.com',
+            'password' => Hash::make('0554'),
+            'active' => true,
+        ]);
+    }
+}
